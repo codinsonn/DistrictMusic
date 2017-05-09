6a5d8f5f2431ceb9c19308f826d2e7920c6df6bb
Thorr Stevens <ThorrStevens@Thorrs-MacBook-Pro.local>
2017-05-09 13:34:17 +0200

    fixed untracked files

--- server/config/index.js
+++
@@ -1,7 +0,0 @@
-require("rootpath")();
-var _ = require("lodash");
-
-module.exports = _.merge(
-  require(__dirname + "/env/general.js"),
-  require(__dirname + "/env/" + process.env.NODE_ENV + ".js" || {})
-);
